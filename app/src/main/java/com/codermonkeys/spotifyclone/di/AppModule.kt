40cb7f13ac04684a6e470ee34a664ff680f76cf0
jajendrapandeya <=>
2020-10-28 11:41:43 +0545

    Dagger Hilt Setup

---
+++ app/src/main/java/com/codermonkeys/spotifyclone/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.codermonkeys.spotifyclone.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.codermonkeys.spotifyclone.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+//this module will be installed in AppComponent i.e it will remain as long as app lives
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+            RequestOptions()
+                .placeholder(R.drawable.ic_image)
+                .error(R.drawable.ic_image)
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+        )
+}
