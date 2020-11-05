a4b50d43db7171f12c86b5bce937ae6a5ab4e02e
jajendrapandeya <=>
2020-11-05 10:42:02 +0545

    Finished Firebase Music Source

--- app/src/main/java/com/codermonkeys/spotifyclone/di/ServiceModule.kt
+++ app/src/main/java/com/codermonkeys/spotifyclone/di/ServiceModule.kt
@@ -1,6 +1,7 @@
 package com.codermonkeys.spotifyclone.di
 
 import android.content.Context
+import com.codermonkeys.spotifyclone.data.remote.MusicDatabase
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
@@ -16,6 +17,10 @@ import dagger.hilt.android.scopes.ServiceScoped
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideMusicDatabase() = MusicDatabase()
 
     @ServiceScoped
     @Provides
