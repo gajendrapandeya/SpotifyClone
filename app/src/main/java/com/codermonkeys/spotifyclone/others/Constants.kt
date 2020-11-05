7ffefc41d3c947fab77bbaf46b1ee53c964ab816
jajendrapandeya <=>
2020-11-05 13:20:07 +0545

    Setting Up the Music Notification

--- app/src/main/java/com/codermonkeys/spotifyclone/others/Constants.kt
+++ app/src/main/java/com/codermonkeys/spotifyclone/others/Constants.kt
@@ -3,4 +3,7 @@ package com.codermonkeys.spotifyclone.others
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
 }
